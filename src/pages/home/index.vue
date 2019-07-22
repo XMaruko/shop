201f3d7696300ba039fe4c424eccd6369f4e6ad4
XMaruko <1540587645@qq.com>
2019-07-22 20:55:03 +0800

    搜索框配置完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>首页</div>
+  <div>
+    <div class="search">
+      <div class="searchBar">
+        <icon type="search"></icon>
+        <span>搜索</span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -9,4 +16,25 @@ export default {
 };
 </script>
 <style lang="" scope>
+div .search {
+  background-color: #eb4450;
+  height: 35px;
+  padding: 10px;
+}
+div .search .searchBar {
+  background-color: #fff;
+  width: 355px;
+  height: 30px;
+  text-align: center;
+  padding: 3px;
+  box-sizing: border-box;
+}
+div .search .searchBar icon {
+  vertical-align: middle;
+}
+div .search .searchBar span {
+  color: #666;
+  font-size: 14px;
+  margin-left: 5px;
+}
 </style>
